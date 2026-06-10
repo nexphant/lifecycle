2e962acdac34a3af113a733d0526ec4f17fdb0b8
rawp-id <wpipit35@gmail.com>
2026-06-10 14:34:00 +0700

    feat: Remove deprecated scheduling commands and clean up related classes

--- src/JobOwner.php
+++ src/JobOwner.php
@@ -4,12 +4,9 @@ namespace Nexph\Lifecycle;
 
 class JobOwner extends AbstractOwner
 {
-    private mixed $job;
-    
     public function __construct(mixed $job = null, ?OwnerScope $parent = null)
     {
         parent::__construct($parent);
-        $this->job = $job;
     }
     
     public function spawn(callable $fn): ChildFiberOwner
--- src/TaskOwner.php
+++ src/TaskOwner.php
@@ -4,12 +4,9 @@ namespace Nexph\Lifecycle;
 
 class TaskOwner extends AbstractOwner
 {
-    private mixed $task;
-    
     public function __construct(mixed $task = null, ?OwnerScope $parent = null)
     {
         parent::__construct($parent);
-        $this->task = $task;
     }
     
     public function spawn(callable $fn): ChildFiberOwner

75c4670492d80895576449d661673e91f42bf331
rawp-id <wpipit35@gmail.com>
2026-06-26 10:37:47 +0700

    feat: enhance route compilation with hash validation and optimize container method resolution

--- src/Lifecycle.php
+++ src/Lifecycle.php
@@ -4,29 +4,29 @@ namespace Nexphant\Lifecycle;
 
 class Lifecycle
 {
-    public static function worker(): WorkerOwner
+    public static function worker(?OwnerScope $parent = null): WorkerOwner
     {
-        return new WorkerOwner();
+        return new WorkerOwner($parent);
     }
 
-    public static function request(): RequestOwner
+    public static function request(?OwnerScope $parent = null): RequestOwner
     {
-        return new RequestOwner();
+        return new RequestOwner($parent);
     }
 
-    public static function job($job): JobOwner
+    public static function job(mixed $job = null, ?OwnerScope $parent = null): JobOwner
     {
-        return new JobOwner();
+        return new JobOwner($job, $parent);
     }
 
-    public static function task($task): TaskOwner
+    public static function task(mixed $task = null, ?OwnerScope $parent = null): TaskOwner
     {
-        return new TaskOwner();
+        return new TaskOwner($task, $parent);
     }
 
-    public static function message(): MessageOwner
+    public static function message(?OwnerScope $parent = null): MessageOwner
     {
-        return new MessageOwner();
+        return new MessageOwner($parent);
     }
 
     public static function childFiber(?OwnerScope $parent = null): ChildFiberOwner
